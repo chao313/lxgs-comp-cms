f5b5d8361af24abdc16ea5a789280c1a6cce7903
liujie1988 <943450574@qq.com>
2017-05-05 09:35:25 +0800

    协议模板dubbo接口，以及实现

---
+++ sdxd-comp-cms-api/src/main/java/com/sdxd/cms/dubbo/api/request/QueryAgreementTemplateRequest.java
@@ -0,0 +1,15 @@
+package com.sdxd.cms.dubbo.api.request;
+
+import com.sdxd.framework.dubbo.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author liujie
+ * @Date 2017/5/4
+ * 盛大小贷
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class QueryAgreementTemplateRequest extends PageRequest {
+}
---
+++ sdxd-comp-cms-test/src/main/java/com/sdxd/cms/AgreementTemplateService.java
@@ -0,0 +1,42 @@
+package com.sdxd.cms;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.sdxd.cms.dubbo.api.AgreementTemplateDubboService;
+import com.sdxd.cms.dubbo.api.pojo.AgreementTemplateVo;
+import com.sdxd.cms.dubbo.api.request.AgreementTemplateRequest;
+import com.sdxd.cms.dubbo.api.request.DeleteAgreementTemplateRequest;
+import com.sdxd.cms.dubbo.api.request.QueryAgreementTemplateRequest;
+import com.sdxd.cms.dubbo.api.response.AgreementTemplateResponse;
+import com.sdxd.cms.dubbo.api.response.DeleteAgreementTemplateResponse;
+import com.sdxd.framework.dto.PaginationSupport;
+import com.sdxd.framework.dubbo.DubboResponse;
+import javafx.scene.control.Pagination;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liujie
+ * @Date 2017/5/4
+ * 盛大小贷
+ */
+@Service
+public class AgreementTemplateService {
+
+    @Reference(version = "1.0.0")
+    private AgreementTemplateDubboService agreementTemplateDubboService;
+
+    public DubboResponse<AgreementTemplateResponse> create (AgreementTemplateRequest  request){
+        return agreementTemplateDubboService.createAgreementTemplate(request);
+    }
+
+    public  DubboResponse<DeleteAgreementTemplateResponse> delete(DeleteAgreementTemplateRequest request){
+        return agreementTemplateDubboService.deleteAgreementTemplate(request);
+    }
+
+    public DubboResponse<PaginationSupport<AgreementTemplateVo>> list (QueryAgreementTemplateRequest request){
+        return agreementTemplateDubboService.queryAgreementTemplate(request);
+    }
+
+    public DubboResponse<Integer> count (QueryAgreementTemplateRequest request){
+        return agreementTemplateDubboService.queryAgreementTemplateCount(request);
+    }
+}
